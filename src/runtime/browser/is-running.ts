c061985419cdc3a2c18e2a1f9b6e508f523635af
Fabian Cook <hello@fabiancook.dev>
2023-11-17 15:35:49 +1300

    Archive opennetwork/environment

--- src/runtime/browser/is-running.ts
+++
@@ -1,9 +0,0 @@
-import { isRunning as isRunningDeno } from "../deno/is-running";
-
-export function isRunning() {
-    return typeof localStorage !== "undefined" && typeof addEventListener !== "undefined" && !isRunningDeno()
-}
-
-export function isRunningServiceWorker() {
-    return isRunning() && typeof caches !== "undefined";
-}
